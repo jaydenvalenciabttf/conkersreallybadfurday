847462b4ab57a19d7a74fa06301822a017575c81
jaydenvalenciabttf <jaydenvalenciabttf@gmail.com>
2026-04-01 20:19:38 -0700

    Update romlist.js

--- romlist.js
+++ romlist.js
@@ -1,1 +1,3 @@
-var ROMLIST = [ {url: "https://www.mediafire.com/file/t9gllv3l8ljsa2e/Conker's+Bad+Fur+Day+(USA).z64/file", title: "Conker's Bad Fur Day"} ];
+var ROMLIST = [
+    {url: "https://www.mediafire.com/file/t9gllv3l8ljsa2e/Conker's+Bad+Fur+Day+(USA).z64/file", title: "Conker's Bad Fur Day"}
+];
